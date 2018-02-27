477e03e94c93acc9b62550517aff2e082141b246
Greg Douglas <gwshark0@gmail.com>
2018-02-27 10:53:46 -0500

    Use router props instead of state for current path

--- src/components/App.js
+++ src/components/App.js
@@ -27,13 +27,13 @@ function getFirstPath(flow = []) {
   return flow[0];
 }
 
-function getPreviousPath(flow = [], currentStep) {
-  const index = flow.indexOf(currentStep);
+function getPreviousPath(flow = [], currentPath) {
+  const index = flow.indexOf(currentPath);
   return (index >= 0) ? flow[index - 1] : undefined;
 }
 
-function getNextPath(flow = [], currentStep) {
-  const index = flow.indexOf(currentStep);
+function getNextPath(flow = [], currentPath) {
+  const index = flow.indexOf(currentPath);
   return (index >= 0) ? flow[index + 1] : undefined;
 }
 
@@ -45,27 +45,17 @@ class App extends Component {
     const query = queryString.parse(this.props.location.search);
 
     this.state = {
-      currentPath: this.props.location.pathname,
       flow: getFlow(query.flow),
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.location !== nextProps.location) {
-      this.onRouteChanged(nextProps.location.pathname);
-    }
-  }
-
-  onRouteChanged(nextPath) {
-    this.setState({ currentPath: nextPath });
-  }
-
   render() {
-    const { flow, currentPath } = this.state;
+    const { flow } = this.state;
+    const { pathname, search } = this.props.location;
+
     const firstPath = getFirstPath(flow);
-    const previousPath = getPreviousPath(flow, currentPath);
-    const nextPath = getNextPath(flow, currentPath);
-    const search = this.props.location.search;
+    const previousPath = getPreviousPath(flow, pathname);
+    const nextPath = getNextPath(flow, pathname);
 
     return (
       <div className="app">
