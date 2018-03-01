36c57975d17ed995adc02cd72326893a17431f86
Greg Douglas <gwshark0@gmail.com>
2018-02-28 23:26:19 -0500

    Persist store in local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux'
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
@@ -15,17 +18,26 @@ const history = createHistory();
 
 const middleware = routerMiddleware(history);
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
 const store = createStore(
-  reducer,
+  persistReducer(persistConfig, reducer),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(middleware)
 );
 
+const persistor = persistStore(store);
+
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
